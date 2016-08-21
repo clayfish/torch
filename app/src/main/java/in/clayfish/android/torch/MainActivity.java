d270e1a31056e54065ea8c40a9e99ce8cbafb6b3
Nishtha Mishra <nishtha@clay.fish>
2016-08-22 02:08:45 +0530

    set a button, uploaded on google play

--- app/src/main/java/in/clayfish/android/torch/MainActivity.java
+++ app/src/main/java/in/clayfish/android/torch/MainActivity.java
@@ -1,11 +1,12 @@
 package in.clayfish.android.torch;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
         AdView mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+
+        Button button = (Button) findViewById(R.id.toggleButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
