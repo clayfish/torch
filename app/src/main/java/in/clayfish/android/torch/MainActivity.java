aaa73941ffc57c1326c6ff266ff1ad3eaef01300
Alok Shukla <alok@clay.fish>
2016-08-28 19:42:16 +0530

    #4 added button png

--- app/src/main/java/in/clayfish/android/torch/MainActivity.java
+++ app/src/main/java/in/clayfish/android/torch/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     private boolean flashOn;
     private Camera camera = null;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
